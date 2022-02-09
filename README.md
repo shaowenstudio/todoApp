cea60b91f0eb16bd412ea4511c2b7e7ebf0d4c85
shawn <shaowenwork@gmail.com>
2022-02-08 22:36:59 -0800

    2nd commit.

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-[React-Redux-Saga-*？晕了？一篇文章弄懂的实战指南！](https://zhuanlan.zhihu.com/p/82442831)的相关代码。
+本 repo 摘自[React-Redux-Saga-*？晕了？一篇文章弄懂的实战指南！](https://zhuanlan.zhihu.com/p/82442831)的相关代码。
 
-如果大家有新的想法，欢迎pull或者fork自己玩。
-
-
-Happy coding!
-
-
-
-Zhe.
+是一个 learning project
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,9 +5,15 @@ import { LOAD_TODO_LIST, RENDER_TODO_LIST } from '../actions';
 
 export function* fetchToDoList() {
   const endpoint =
-    'https://gist.githubusercontent.com/brunokrebs/' +
-    'f1cacbacd53be83940e1e85860b6c65b/raw/to-do-items.json';
-  const response = yield call(fetch, endpoint);
+    // 'https://gist.githubusercontent.com/brunokrebs/f1cacbacd53be83940e1e85860b6c65b/raw/to-do-items.json';
+    'https://tnl21xz29a.execute-api.us-west-2.amazonaws.com/p1-backend';
+  const args = [
+    endpoint,
+  ]
+  // FETCH API https://developer.mozilla.org/zh-TW/docs/Web/API/Fetch_API/Using_Fetch
+  const response = yield call(fetch, args);
+  // const response = yield call(fetch, endpoint);
+  console.log(response);
   const data = yield response.json();
   yield put({ type: RENDER_TODO_LIST, toDoList: data });
 }
